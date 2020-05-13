03937bf7b5a8f1e537744dd023ed3f1c135ad6ed
joyceeesmile <65269205+joyceeesmile@users.noreply.github.com>
2020-05-13 11:48:10 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+let mySprite = sprites.create(img`
+e e e . . . . e e e . . . . 
+c d d c . . c d d c . . . . 
+c b d d f f d d b c . . . . 
+c 3 b d d b d b 3 c . . . . 
+f b 3 d d d d 3 b f . . . . 
+e d d d d d d d d e . . . . 
+e d f f d d f f d e . b f b 
+f d d f d d f d d f . f d f 
+f b d d b b d d c f . f d f 
+. f c c c c c c b b f f d f 
+. f b d d d d d d b b d b f 
+. f d d d d d b d d f f f f 
+. f d f f f d f f d f . . . 
+. f f f . f f f . f f . . . 
+`, SpriteKind.Player)
+scene.setBackgroundColor(9)
